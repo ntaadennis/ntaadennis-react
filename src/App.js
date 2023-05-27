cf2996dc5468784a7e83823cf28c50cf0626ab88
ntaadennis <ntaadennis@gmnail.com>
2023-05-28 01:32:34 +0200

    add iframe prop

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import './App.css';
 import StreamLink from './components/StreamLink.js'
 import * as React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <StreamLink name='BUFFSTREAMS' link="https://streameast.top/match/f1/monaco-gp/203655"/>
-      <StreamLink name='BEST SOLARIS' link="https://bestsolaris.com/solaris.php?postid=30876"/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <StreamLink name='BUFFSTREAMS' link="https://streameast.top/match/f1/monaco-gp/203655"/>
+        <StreamLink name='BEST SOLARIS' link="https://bestsolaris.com/solaris.php?postid=30876" iframe/>
+  
+      </div>
+    );
+  }
 
-    </div>
-  );
 }
 
 export default App;
--- src/components/StreamLink.js
+++ src/components/StreamLink.js
@@ -1,34 +1,45 @@
 import * as React from 'react';
 
-function StreamLink(props) {
-    const [open, setOpen] = React.useState(false);
-    const [iframe, setIframe] = React.useState(false);
+class StreamLink extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { 
+            open: false,
+        };
 
-    const handleOpen = () => {
-        setOpen(!open);
+        this.handleOpen = this.handleOpen.bind(this);
+
+      }
+      
+    handleOpen = () => {
+        this.setState({open: !this.state.open})
     }
 
-    return (
-        <div>
-            <div class="dropdown-container">
-                <button onClick={handleOpen} class="dropdown-trigger">{props.name}</button>
-                    { open ?
-                        <div class="dropdown-content" id="dropdown-content">
-                            { iframe?
-                            <iframe frameborder='0' height='100%' width='100%' src="https://bestsolaris.com/solaris.php?postid=30876" allowfullscreen allowtransparency>
-                            </iframe>
-                            : 
-                            <div class="watch-here">
-                                <a href={props.link} target="blank">watch here</a>    
+    render() {
+        return (
+            <div>
+                <div class="dropdown-container">
+                    <button onClick={this.handleOpen} class="dropdown-trigger">{this.props.name}</button>
+                        {this.state.open ?
+                            <div class="dropdown-content" id="dropdown-content">
+                                { this.props.iframe ?
+                                <iframe title='stream' frameborder='0' height='100%' width='100%' src="https://bestsolaris.com/solaris.php?postid=30876" allowfullscreen allowtransparency>
+                                </iframe>
+                                : 
+    
+                                <div class="watch-here">
+                                    <a href={this.props.link} target="blank">watch here</a>    
+                                </div>
+                                }
                             </div>
-                            }
-                        </div>
-                    : <span/> }
-            </div >
-        </div>
+                        : <span/> }
+                </div >
+            </div>
+        )
+      }
+    
         
-    )
 }
 
 export default StreamLink;
